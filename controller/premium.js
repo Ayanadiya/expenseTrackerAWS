3bf0362fe19a1c380f7522925aebb3f9ea179f7a
Ayanadiya <ayanadiya17@gmail.com>
2024-11-21 11:27:41 +0530

    leaderboard optimisation

--- controller/premium.js
+++ controller/premium.js
@@ -1,36 +1,29 @@
-const Leaderboard=require('../models/leaderboard');
+
 const Expense=require('../models/expenses');
 const User=require('../models/User');
+const sequelize = require('../utils/database');
+const Sequelize= require('sequelize');
 
 exports.getleaderboard = async (req,res,next) => {
     try {
-          const users= await User.findAll();
-          const expenses= await Expense.findAll();
-          const totalexpensebyuserid={};
-          for(expense of expenses)
-          {
-            if(expense.userId in totalexpensebyuserid)
-            {
-                totalexpensebyuserid[expense.userId]= totalexpensebyuserid[expense.userId]+expense.amount;
-            }
-            else
-            {
-                totalexpensebyuserid[expense.userId]= expense.amount;
-
-            }
-          }
-          const leaderboard1=[];
-          for(user of users)
-          {
-                leaderboard1.push({name:user.name, total:totalexpensebyuserid[user.id] || 0})
-          }
-          leaderboard1.sort((a,b) => b.total-a.total);
-          console.log(leaderboard1)
-          res.status(200).json(leaderboard1);
+          const leaderboard= await Expense.findAll({
+            attributes:[
+                'userid',
+                [Sequelize.fn('SUM', sequelize.col('expenses.amount')),'totalamount']
+             ],
+             include:[{
+                model:User,
+                attributes:['id', 'name']
+             }],
+             group:[ 'expenses.userId'],
+             order:[['totalamount', 'DESC']]
+          })
+          console.log(leaderboard)
+          res.status(200).json(leaderboard);
         }
 
     catch (error) {
-        console.log(err);
+        console.log(error);
         throw new Error(error);
     }
 }
